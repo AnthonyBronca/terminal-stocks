ebab935edb91700e9f2177e49c6b651b41dfbc79
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2023-01-08 22:40:51 -0500

    added files and env variables

--- javascript/terminal-stock.js
+++ javascript/terminal-stock.js
@@ -1,1 +1,5 @@
-var number = 1;
+//alias to compile this file: compile
+require('dotenv').config();
+var alpaca_key = process.env.ALPACA_KEY;
+var alpaca_secret_key = process.env.ALPACA_SECRET_KEY;
+console.log(alpaca_secret_key);
